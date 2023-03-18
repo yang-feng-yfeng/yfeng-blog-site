4a95f9a30f423cf9238ec5a138385b9f38a65506
Yang Feng <49683353+yang-feng-yfeng@users.noreply.github.com>
2023-03-18 17:11:29 +0000

    Fix icon

--- internal/testing/__mocks__/author.ts
+++ internal/testing/__mocks__/author.ts
@@ -1,7 +1,7 @@
 import contacts from "./contacts";
 
 export default {
-  photo: "/2.JPG",
+  photo: "/1_256.png",
   bio: "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
   name: "John Doe",
   contacts,
